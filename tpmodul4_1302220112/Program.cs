cd3d31d353b3c1c38112b9c8de072583ac216ef9
Syauqi <syauqi.d.u@gmail.com>
2024-03-10 12:03:30 +0700

    menambahkan implementasi dengan table driven

--- tpmodul4_1302220112/Program.cs
+++ tpmodul4_1302220112/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static KodePos;
+
+public class KodePos
+{
+    public string[] kelurahan = { "Batununggal", "Kujangsari", "Mengger", "Wates", 
+        "Cijaura", "Jatisari", "Margasari", "Sekejati", "Kebonwaru", "Maleer", "Samoja" };
+    public int[] kodePos = { 40266, 40287, 40267, 40256, 40287, 40286, 40286, 40286, 40272, 40274, 40273 };
+    public int getKodePos(int i)
+    {
+        return kodePos[i];
+    }
+}
+
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        KodePos kodePos = new KodePos();
+        for (int i = 0; i < kodePos.kelurahan.Length; i++)
+        {
+            Console.WriteLine("Kode Pos Kelurahan " + kodePos.kelurahan[i] + ": " + kodePos.getKodePos(i));
+        }
+    }
+}
